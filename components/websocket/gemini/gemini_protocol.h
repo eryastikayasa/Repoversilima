2075914b12674eb0c81007971d9e6150ef920fbe
eryastikayasa <eryastikayasa@gmail.com>
2026-09-10 21:11:12 +0800

    gemini: add realtime PCM audio message builder

--- components/websocket/gemini/gemini_protocol.h
+++ components/websocket/gemini/gemini_protocol.h
@@ -2,12 +2,17 @@
 
 #include <stdbool.h>
 #include <stddef.h>
+#include <stdint.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 bool gemini_protocol_build_setup(char **output, size_t *output_len);
+bool gemini_protocol_build_realtime_audio(const int16_t *pcm16,
+                                          size_t samples,
+                                          char **output,
+                                          size_t *output_len);
 bool gemini_protocol_process_message(const char *json, size_t len);
 
 #ifdef __cplusplus
