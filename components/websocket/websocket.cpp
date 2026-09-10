4887a38e667ff925778b8aac3da9f25696fe4815
eryastikayasa <eryastikayasa@gmail.com>
2026-09-10 18:32:31 +0800

    Add WebSocket manager skeleton

---
+++ components/websocket/websocket.cpp
@@ -0,0 +1,32 @@
+#include "websocket.h"
+#include "websocket_transport.h"
+
+esp_err_t websocket_init(void)
+{
+    return websocket_transport_init();
+}
+
+esp_err_t websocket_connect(void)
+{
+    return websocket_transport_connect();
+}
+
+esp_err_t websocket_disconnect(void)
+{
+    return websocket_transport_disconnect();
+}
+
+bool websocket_is_connected(void)
+{
+    return websocket_transport_is_connected();
+}
+
+esp_err_t websocket_send_text(const char *text, size_t len)
+{
+    return websocket_transport_send_text(text, len);
+}
+
+esp_err_t websocket_send_binary(const uint8_t *data, size_t len)
+{
+    return websocket_transport_send_binary(data, len);
+}
