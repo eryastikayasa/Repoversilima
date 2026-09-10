300a927944bf9c9773dabc61f7bd88722b321809
eryastikayasa <eryastikayasa@gmail.com>
2026-09-10 18:32:27 +0800

    Add WebSocket transport API skeleton

---
+++ components/websocket/include/websocket_transport.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include "esp_err.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+esp_err_t websocket_transport_init(void);
+esp_err_t websocket_transport_connect(void);
+esp_err_t websocket_transport_disconnect(void);
+bool websocket_transport_is_connected(void);
+esp_err_t websocket_transport_send_text(const char *text, size_t len);
+esp_err_t websocket_transport_send_binary(const uint8_t *data, size_t len);
+
+#ifdef __cplusplus
+}
+#endif
