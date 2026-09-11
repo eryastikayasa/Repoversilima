4ec80a44db3316e46e86347a8f7d6cc38ac744de
eryastikayasa <eryastikayasa@gmail.com>
2026-09-11 22:24:21 +0800

    fix: include esp_timer for playback drain

--- components/audio_engine/audio_engine.cpp
+++ components/audio_engine/audio_engine.cpp
@@ -4,6 +4,7 @@
 #include "esp_heap_caps.h"
 #include "esp_log.h"
 #include "esp_err.h"
+#include "esp_timer.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/queue.h"
