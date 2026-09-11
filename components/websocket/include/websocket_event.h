98e1bedc1e3190a278d9eb96b9bc6ed37820d77c
eryastikayasa <eryastikayasa@gmail.com>
2026-09-11 20:38:59 +0800

    websocket: initialize RX worker outside event callback

--- components/websocket/include/websocket_event.h
+++ components/websocket/include/websocket_event.h
@@ -7,6 +7,8 @@
 extern "C" {
 #endif
 
+bool websocket_event_init(void);
+
 void websocket_event_handler(void *handler_args,
                              esp_event_base_t base,
                              int32_t event_id,
