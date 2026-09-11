deee15b3a8d92b3fca8970f0525dc249717a7e32
eryastikayasa <eryastikayasa@gmail.com>
2026-09-11 19:19:41 +0800

    Add Wi-Fi link diagnostic API

--- components/wifi_manager/include/wifi_manager.h
+++ components/wifi_manager/include/wifi_manager.h
@@ -14,3 +14,6 @@ void wifi_init_sta(void);
 bool wifi_wait_for_connection(uint32_t timeout_ms);
 
 bool wifi_is_ready(void);
+
+// Diagnostic-only snapshot of the current STA link. Does not alter Wi-Fi state.
+void wifi_log_diagnostic(void);
