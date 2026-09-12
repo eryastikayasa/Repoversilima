cd241bec1a658e4d81b336124fb1651e702a41e7
eryastikayasa <eryastikayasa@gmail.com>
2026-09-12 13:41:05 +0800

    Finalize AudioEngine public interface

--- components/audio_engine/include/audio_engine.h
+++ components/audio_engine/include/audio_engine.h
@@ -1,36 +1,25 @@
 #pragma once
-
+#include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
-#include <stdbool.h>
-
 #ifdef __cplusplus
 extern "C" {
 #endif
-
+typedef enum { AUDIO_ENGINE_IDLE=0, AUDIO_ENGINE_LISTENING, AUDIO_ENGINE_PLAYING, AUDIO_ENGINE_ERROR } audio_engine_state_t;
+typedef void (*audio_engine_mic_frame_cb_t)(const uint8_t *pcm, size_t len, void *ctx);
 bool audio_engine_init(void);
-bool audio_engine_start_wakeword(void);
-void audio_engine_stop_wakeword(void);
-bool audio_engine_process_wakeword(void);
-bool audio_engine_wakeword_detected(void);
-void audio_engine_clear_wakeword(void);
-void audio_engine_stop(void);
-bool audio_engine_start_conversation(void);
-void audio_engine_stop_conversation(void);
-bool audio_engine_conversation_active(void);
-bool audio_engine_read_mic_frame(int16_t *buffer, size_t samples, uint32_t timeout_ms);
-size_t audio_engine_mic_frame_samples(void);
-bool audio_engine_start_playback(void);
-void audio_engine_stop_playback(void);
-bool audio_engine_playback_active(void);
-
-/** Drain all queued speaker PCM, then release the speaker. */
-bool audio_engine_drain_playback(void);
-
-bool audio_engine_write_speaker_pcm(const int16_t *buffer,
-                                    size_t samples,
-                                    uint32_t timeout_ms);
-
+audio_engine_state_t audio_engine_get_state(void);
+const char *audio_engine_state_name(audio_engine_state_t state);
+bool audio_engine_set_mic_listener(audio_engine_mic_frame_cb_t cb, void *ctx);
+bool audio_engine_set_mic_sink(audio_engine_mic_frame_cb_t cb, void *ctx);
+bool audio_engine_start_capture(void);
+void audio_engine_stop_capture(void);
+bool audio_engine_capture_active(void);
+bool audio_engine_push_model_audio(const uint8_t *pcm, size_t len, uint32_t generation);
+bool audio_engine_push_model_audio_base64(const char *b64, size_t len, uint32_t generation);
+void audio_engine_start_input_session(void);
+void audio_engine_stop_input_session(void);
+bool audio_engine_input_session_active(void);
 #ifdef __cplusplus
 }
 #endif
