cb912262aa6775a323438750e4ad1e14994214bf
eryastikayasa <eryastikayasa@gmail.com>
2026-09-17 23:48:08 +0800

    Align Repo5 display engine header with Repo6

--- components/display/display_engine/display_engine.h
+++ components/display/display_engine/display_engine.h
@@ -9,6 +9,8 @@ extern "C" {
 void display_engine_init(void);
 void display_engine_start(void);
 void display_engine_stop(void);
+
+/* Single lightweight presentation entry point for system state. */
 void display_set_system_state(face_state_t face, const char *status);
 
 #ifdef __cplusplus
